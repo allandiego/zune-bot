a89e310479fee4c9c8b83bc7d1b44a7a7e4d3c11
Diego Rodrigues <allan_diego@hotmail.com>
2020-11-11 14:02:32 -0300

    fix: babel.config

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,6 @@ module.exports = {
       }
     }],
     'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['transform-remove-console', { exclude: ['error', 'warn'] }]
   ],
